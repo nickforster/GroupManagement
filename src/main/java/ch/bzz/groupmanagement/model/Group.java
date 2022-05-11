3bd3a1d118a2f505ad43b5da5fff50129b7830b5
Nick Forster <forstern@bzz.ch>
2022-05-11 15:44:19 +0200

    model and service added

---
+++ src/main/java/ch/bzz/groupmanagement/model/Group.java
@@ -0,0 +1,45 @@
+package ch.bzz.groupmanagement.model;
+
+import java.util.List;
+
+/**
+ * A group in the group Management
+ */
+public class Group {
+    private String title;
+    private String description;
+    private int graduationYear;
+    private List<Student> students;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getGraduationYear() {
+        return graduationYear;
+    }
+
+    public void setGraduationYear(int graduationYear) {
+        this.graduationYear = graduationYear;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
---
+++ src/main/java/ch/bzz/groupmanagement/model/Student.java
@@ -0,0 +1,45 @@
+package ch.bzz.groupmanagement.model;
+
+import java.time.LocalDate;
+
+/**
+ * A student in a group
+ */
+public class Student {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
